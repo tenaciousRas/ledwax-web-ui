175f2f0fc176acaba7f3c019087a275f5fec8382
Free Beachler <fbeachler@gmail.com>
2016-04-20 21:55:06 -0600

    fixed karma tests and ng setup ctrl unit test;

--- src/modules/public-ledwax-web-ui/app/controllers/setup_test.js
+++ src/modules/public-ledwax-web-ui/app/controllers/setup_test.js
@@ -7,12 +7,16 @@ describe('LEDWAXW3.setup module', function() {
 	describe('setup controller', function() {
 		var scope, ctrl, configService, restService;
 
-		it('should load ...', inject(function($rootScope, $scope, $cookies,
+		it('should load ...', inject(function($controller, $rootScope, $cookies,
 				$sanitize, $translate, Config, REST_IoT) {
 			scope = $rootScope.$new();
 			restService = REST_IoT;
 			ctrl = $controller('SetupCtrl', {
+				$rootScope : $rootScope,
 				$scope : scope,
+				$cookies : $cookies,
+				$sanitize : $sanitize,
+				$translate : $translate,
 				REST_IoT : restService
 			});
 			expect(ctrl).toBeDefined();
